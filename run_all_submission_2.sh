289a53ef8553a3943bb815c69f2b758f56e170ae
Nathan Clark <nc8774@gmail.com>
2024-10-28 15:00:43 -0400

    add mkdir to run_all

--- run_all_submission_2.sh
+++ run_all_submission_2.sh
@@ -1,8 +1,10 @@
 # Assumes python already references version 3.11.7
 python -m pip install -r requirements.txt
+mkdir data
 cd data
 kaggle competitions download -c cs-506-midterm-fall-2024
 cd ..
+mkdir cache
 python preprocess_data.py
 wget https://nlp.stanford.edu/data/wordvecs/glove.840B.300d.zip
 python fuzzy_ngram_matrix_factorization.py
